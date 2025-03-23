86435d7a7ff8306651dc947da7ebcc71301c53b9
ramadanomar <ramadan.omaarr@gmail.com>
2025-03-23 23:58:56 +0200

    bug: log out of wordpress as well

--- tutorindigo/templates/indigo/lms/static/js/logout.js
+++ tutorindigo/templates/indigo/lms/static/js/logout.js
@@ -2,32 +2,42 @@
  * JS for the logout page.
  *
  * This script waits for all iframes on the page to load before redirecting the user
- * to the Finlearn logout URL, which in turn redirects the user to the specified URL.
+ * to the WordPress logout URL.
  */
-(function($) {
-    'use strict';
+(function ($) {
+  "use strict";
 
-    $(function() {
-        var $iframeContainer = $('#iframeContainer'),
-            $iframes = $iframeContainer.find('iframe'),
-            // The final destination after logout.
-            redirectUrl = $iframeContainer.data('redirect-url'),
-            // Base Finlearn logout URL.
-            logoutUrl = "https://auth.finlearn.ro/realms/prod/protocol/openid-connect/logout?client_id=openedx";
+  $(function () {
+    var $iframeContainer = $("#iframeContainer"),
+      $iframes = $iframeContainer.find("iframe"),
+      // WordPress logout URL
+      wordpressLogoutUrl = "http://finlearn.ro/wp-login.php?action=logout",
+      // Base Finlearn logout URL.
+      finlearnLogoutUrl =
+        "https://auth.finlearn.ro/realms/prod/protocol/openid-connect/logout?client_id=openedx";
 
-        // Append the redirect URI so that after logout the user is sent to redirectUrl.
-        logoutUrl += "&redirect_uri=" + encodeURIComponent(redirectUrl);
+    // Redirect to WordPress logout URL directly
+    function redirectToWordpressLogout() {
+      window.location.href = wordpressLogoutUrl;
+    }
 
-        function doRedirect() {
-            window.location.href = logoutUrl;
-        }
+    // If you need to logout from both systems, uncomment this function and use it instead
+    // Append the WordPress logout as the redirect URI so user is sent there after Finlearn logout
+    function redirectViaFinlearn() {
+      var logoutUrl = finlearnLogoutUrl;
+      logoutUrl += "&redirect_uri=" + encodeURIComponent(wordpressLogoutUrl);
+      window.location.href = logoutUrl;
+    }
 
-        if ($iframes.length === 0) {
-            doRedirect();
-        } else {
-            $iframes.allLoaded(function() {
-                doRedirect();
-            });
-        }
-    });
-}(jQuery));
+    // Choose which redirect function to use based on your requirements
+    var doRedirect = redirectToWordpressLogout; // or redirectViaFinlearn if needed
+
+    if ($iframes.length === 0) {
+      doRedirect();
+    } else {
+      $iframes.allLoaded(function () {
+        doRedirect();
+      });
+    }
+  });
+})(jQuery);
